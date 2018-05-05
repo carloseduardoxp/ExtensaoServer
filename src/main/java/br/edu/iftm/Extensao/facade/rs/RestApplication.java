f380742a6e45b3298f14612a9799cdbd7328ed4e
carloseduardoxp <carloseduardoxpto@gmail.com>
2018-05-05 09:09:33 -0300

    ExtensaoServer - first release

---
+++ src/main/java/br/edu/iftm/Extensao/facade/rs/RestApplication.java
@@ -0,0 +1,8 @@
+package br.edu.iftm.Extensao.facade.rs;
+
+import javax.ws.rs.core.Application;
+import javax.ws.rs.ApplicationPath;
+
+@ApplicationPath("rs")
+public class RestApplication extends Application {
+}
---
+++ src/test/java/Exemplo.java
@@ -0,0 +1,11 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Exemplo {
+
+	@Test
+	public void test() {
+		Assert.fail();
+	}
+
+}
